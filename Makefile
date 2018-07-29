f7eda00aee49b45d04ee7c0d4e67f1f9ff3b39a2
Maximilian Ott <maximilian.o.ott@fau.de>
2018-07-29 21:36:42 +0200

    Add test case

--- Makefile
+++ Makefile
@@ -3,3 +3,7 @@
 build/queue.o: src/queue.c
 	mkdir -p build
 	gcc -I include/ -c src/queue.c -o build/queue.o
+
+build/main.o: src/main.c
+	mkdir -p build
+	gcc -I include/ -c src/main.c -o build/main.o
---
+++ src/main.c
@@ -0,0 +1,74 @@
+#include "queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <errno.h>
+
+#define PROBLEM_SIZE 1000000
+
+struct job {
+    pthread_t tid;
+    size_t offset;
+    size_t size;
+};
+
+static void *worker(void *arg){
+	struct job *job = (struct job *) arg;
+	for(int i = job->offset; i < job->size; i++){
+		int *value = malloc(sizeof(int));
+		if(value == NULL){
+			continue;
+		}
+
+		queue_add(value);
+	}
+
+	for(int i = job->offset; i < job->size; i++){
+		int *value = queue_remove();
+		free(value);
+	}
+
+	return NULL;
+}
+
+int main(int argc, char *argv[]) {
+	if(argc != 2){
+		fprintf(stderr, "Usage: %s <number of threads>", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	int numThreads = atoi(argv[1]);
+	struct job *threads = (struct job *) malloc(sizeof(struct job) * numThreads);
+    if(threads == NULL) {
+    	perror("malloc");
+		exit(EXIT_FAILURE);
+	}
+
+	for(size_t i = 0; i < numThreads; i++) {
+		struct job *job  = threads + i;
+		job->offset = (PROBLEM_SIZE / numThreads) * i;
+		
+		if(i == numThreads - 1){
+			job->size = PROBLEM_SIZE - (PROBLEM_SIZE / numThreads) * (numThreads - 1);
+		}else{
+			job->size = (PROBLEM_SIZE / numThreads);
+		}
+
+		errno = pthread_create(&job->tid, NULL, worker, job);
+		if(errno != 0){
+			perror("pthread_create");
+			exit(EXIT_FAILURE);
+		}
+
+		for(size_t i = 0; i < numThreads; i++) {
+			struct job *job = threads + i;
+			if(0 != pthread_join(job->tid, NULL)) {
+				fprintf(stderr, "pthread_join() failed\n");
+			}
+		}
+	}
+		
+	free(threads);
+
+	return 0;
+}
